ec74d2bd327271499ddc17195a410f8fde33a273
Biznatch Enterprises <justin@biznaturally.ca>
2017-07-22 00:07:13 -0500

    Add files via upload

---
+++ example.php
@@ -0,0 +1,55 @@
+<?php
+//###################################################################
+//####################      EZE-CORES 2.0       #####################
+//#      https://github.com/BiznatchEnterprises/EZE-CORES/2.0       #
+//####################   Biznatch Enterprises   #####################
+//####################        Jan, 2017         #####################
+//###################################################################
+
+// @@@@@@@ [EZE-CORES Plugin 1.0]  @@@@@@@
+// https://github.com/BiznatchEnterprises/EZE-CORES/tree/plugin-class
+include('EZE-Cores-Plugin.php'); //     
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// www.domain.com/?page=home     
+// www.domain.com/?page=blog
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ GET/Post Variable [PAGE] Start ~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (isset($_POST["page"]) == TRUE) {
+    $WEB_APP->Change_CurrentPage($_POST["page"]);
+}
+if (isset($PAGE) == FALSE) {
+    if (isset($_POST["page"]) == TRUE) {
+        $WEB_APP->Change_CurrentPage($_POST["page"]);
+    }
+}
+if (isset($PAGE) == FALSE) {
+    if (isset($_GET["page"]) == TRUE) {
+        $WEB_APP->Change_CurrentPage($_GET["page"]);
+    }
+}
+if (isset($PAGE) == FALSE) {
+    if (isset($_GET["PAGE"]) == TRUE) {
+        $WEB_APP->Change_CurrentPage($_GET["PAGE"]);
+    }
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ GET/Post Variable [PAGE] End ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Page Sections Start ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$WEB_APP = new EZE_CORES;                                                                                       // Create new EZE_CORES Object
+$WEB_APP->Change_Settings($SCRIPT_PATH, 'templates/');                                                          // Change General Settings
+
+$TEMPLATEPATH_1 = 'Template-1/';                                                                                // Template #1 Filename
+$TEMPLATEPATH_2 = 'Template-2/';                                                                                // Template #2 Filename
+$WEB_APP->Change_DefaultPage("home");                                                                           // Default Page ID
+
+$WEB_APP->Change_PageSettings('home', 'pages/home/', '', '', '', $TEMPLATEPATH_1);                              // [SetPAGE] Homepage (Default)
+$WEB_APP->Change_PageSettings('blog', 'pages/blog/', '', '', '', $TEMPLATEPATH_2);               		// [SetPAGE] Buy Shares
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Page Sections End ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+?>
